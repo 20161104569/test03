0425fafcb3a6d19a5fd9bd6a2185b4e3ae14eadf
20161104569 <2174037646@qq.com>
2017-06-19 11:58:23 +0800

    从2+....+100=的和
    从2+....+100=的和

--- test03/main.c
+++ test03/main.c
@@ -11,8 +11,11 @@ int main()
 {
     int i,sum;
     sum=0;
-    for(i=1;i<=100;i++)
+    for(i=2;i<=100;i＝i+2)
+    {
         sum=sum+i;
-    printf("1+2+3+....+100=%d",sum);
+    }
+    
+    printf("2+4+6+....+100=%d",sum);
     return 0;
 }

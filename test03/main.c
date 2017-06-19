586c1857c1b21e9d9a54f4aa55317b55207643b5
20161104569 <2174037646@qq.com>
2017-06-19 11:48:30 +0800

    求1+....+100=的和
    求1+....+100=的和

--- test03/main.c
+++ test03/main.c
@@ -6,10 +6,13 @@
 //  Copyright © 2017年 20161104569. All rights reserved.
 //
 
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#include<stdio.h>
+int main()
+{
+    int i,sum;
+    sum=0;
+    for(i=1;i<=100;i++)
+        sum=sum+i;
+    printf("1+2+3+....+100=%d",sum);
     return 0;
 }
